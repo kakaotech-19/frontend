d7c009096c19db0a13ae3230a3e473e72d2b0fde
jaejunday <wowns8114@gmail.com>
2024-10-04 00:40:48 +0900

    #1 feat: 리덕스 스토어 추가

---
+++ store/index.ts
@@ -0,0 +1,29 @@
+import logger from "redux-logger";
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+  reducer: {
+    // user: userReducer,
+    // posts: postsReducer,
+  },
+  middleware: (
+    getDefaultMiddleware: (arg0: {
+      serializableCheck: boolean; // dev: false, prod: true
+      immutableCheck: boolean; // dev: false, prod: true
+    }) => any
+  ) => {
+    const middlewares = getDefaultMiddleware({
+      serializableCheck: process.env.NODE_ENV !== "development", // dev: false, prod: true
+      immutableCheck: process.env.NODE_ENV !== "development", // dev: false, prod: true
+    });
+
+    // Redux Logger 미들웨어를 추가
+    middlewares.push(logger);
+
+    return middlewares;
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export default store;
