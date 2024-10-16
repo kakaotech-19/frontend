b77243bb7a81b3014f076c15bef244a9114e01e3
jaejunday <wowns8114@gmail.com>
2024-10-16 10:07:07 +0900

    feat: Oauth/Email 로그인 토글 폼 추가 feature/TODAK-78/ui-login-signup

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,66 +1,40 @@
 "use client";
 
-import { RootState } from "@/store";
-import { setLoginId, setLoginPassword } from "@/store/slices/login/loginSlice";
-import { Button, HR, Label, TextInput } from "flowbite-react";
-import Link from "next/link";
-import { useDispatch, useSelector } from "react-redux";
+import KakaoLoginButton from "@/components/Auth/KakaoLoginButton";
+import GoogleLoginButton from "@/components/Auth/GoogleLoginButton";
+import { Button, FooterDivider } from "flowbite-react";
+import { useState } from "react";
+import EmailLoginForm from "./EmailLoginForm";
+import { useRouter } from "next/navigation";
 
 const Page = () => {
-  const dispatch = useDispatch();
-  const loginId = useSelector((state: RootState) => state.login.loginId);
-  const password = useSelector((state: RootState) => state.login.password);
-
+  const [isEmailForm, setIsEmailForm] = useState(false);
+  const router = useRouter();
   return (
-    <div className="flex justify-center h-full items-center">
-      <div className="w-80">
-        <form className="flex flex-col gap-4">
-          <div>
-            <div className="mb-2 block">
-              <Label htmlFor="login-id" value="Login ID" />
-            </div>
-            <TextInput
-              id="login-id"
-              type="text"
-              value={loginId}
-              onInput={(e) => dispatch(setLoginId(e.currentTarget.value))}
-              placeholder=""
-              required
-            />
-          </div>
-          <div className="mb-2">
-            <div className="mb-2 block">
-              <Label htmlFor="password1" value="Password" />
-            </div>
-            <TextInput
-              id="password1"
-              type="password"
-              value={password}
-              onInput={(e) => dispatch(setLoginPassword(e.currentTarget.value))}
-              required
-            />
-            <Label
-              htmlFor="forgot-password"
-              className="text-cyan-600 hover:underline dark:text-cyan-500 text-sm"
-              onClick={() => {}}
-            >
-              forgot password?
-            </Label>
-          </div>
-          <Button type="submit">Login</Button>
-          <HR className="mb-0" />
-          <Label htmlFor="signup-link" className="flex">
-            Don't have an account?&nbsp;
-            <Link
-              href="/signup"
-              className="text-cyan-600 hover:underline dark:text-cyan-500"
-            >
-              Create Account
-            </Link>
-          </Label>
-        </form>
-      </div>
-    </div>
+    <>
+      {isEmailForm ? (
+        <EmailLoginForm />
+      ) : (
+        <>
+          <KakaoLoginButton onClick={() => {}} />
+          <br />
+          <GoogleLoginButton onClick={() => {}} />
+          <br />
+          or
+          <FooterDivider />
+          <Button className="w-full" onClick={() => setIsEmailForm(true)}>
+            Sign in with Email
+          </Button>
+          <br />
+          <Button
+            className="w-full bg-white border border-gray-300 text-cyan-600"
+            onClick={() => router.push("/signup")}
+          >
+            Create Account
+          </Button>
+        </>
+      )}
+    </>
   );
 };
 
