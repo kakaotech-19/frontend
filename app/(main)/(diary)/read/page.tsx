464c261fcd30350812417910c6001b1dbf88b53b
jaejunday <wowns8114@gmail.com>
2024-10-21 01:08:23 +0900

    feat: ai코멘트 드래거블로 추가 feature/TODAK-112/UI-navigation-logo

--- app/(main)/(diary)/read/page.tsx
+++ app/(main)/(diary)/read/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import React from "react";
+import Draggable from "react-draggable";
 import Image from "next/image";
+import { Textarea } from "flowbite-react";
 
 const page: React.FC = () => {
   return (
     <div className="w-full min-h-screen flex flex-col justify-center items-center">
-      <div className="w-full mt-14">
+      <div className="w-full flex-col mt-14">
         <div className="flex w-full max-w-md relative" onClick={() => {}}>
           <Image
             width={500}
@@ -19,10 +21,37 @@ const page: React.FC = () => {
           />
         </div>
         <div className="w-full max-w-md">
-          <p className="h-96 text-md p-2 rounded-none border border-red-300">
-            test
+          <p className="text-md h-full p-2 rounded-none border border-red-300 overflow-y-auto">
+            안녕하세요. 여기는 일기 내용입니다. 일기내용 안녕하세요. 여기는 일기
+            내용입니다. 일기내용 안녕하세요. 여기는 일기 내용입니다. 일기내용
+            안녕하세요. 여기는 일기 내용입니다. 일기내용 안녕하세요. 여기는 일기
+            내용입니다. 일기내용 안녕하세요. 여기는 일기 내용입니다. 일기내용
+            안녕하세요. 여기는 일기 내용입니다. 일기내용 안녕하세요. 여기는 일기
+            내용입니다. 일기내용 안녕하세요. 여기는 일기 내용입니다. 일기내용
+            안녕하세요. 여기는 일기 내용입니다. 일기내용 안녕하세요. 여기는 일기
+            내용입니다. 일기내용 안녕하세요. 여기는 일기 내용입니다. 일기내용
+            안녕하세요. 여기는 일기 내용입니다. 일기내용 안녕하세요. 여기는 일기
+            내용입니다. 일기내용 안녕하세요. 여기는 일기 내용입니다. 일기내용
+            안녕하세요. 여기는 일기 내용입니다. 일기내용 안녕하세요. 여기는 일기
+            내용입니다. 일기내용 안녕하세요. 여기는 일기 내용입니다. 일기내용
+            안녕하세요. 여기는 일기 내용입니다. 일기내용 안녕하세요. 여기는 일기
+            내용입니다. 일기내용 안녕하세요. 여기는 일기 내용입니다. 일기내용
+            안녕하세요. 여기는 일기 내용입니다. 일기내용 안녕하세요. 여기는 일기
+            내용입니다. 일기내용 안녕하세요. 여기는 일기 내용입니다. 일기내용
           </p>
         </div>
+        <Draggable>
+          <div className="absolute bottom-20 left-10 flex">
+            <Image
+              width={50}
+              height={50}
+              src="/cat.png"
+              alt={""}
+              className="rounded-full"
+            ></Image>
+            <Textarea readOnly className="ml-2" value={"AI 위로 코멘트다냥"} />
+          </div>
+        </Draggable>
       </div>
     </div>
   );
