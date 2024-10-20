e9c15dadcab578c8b8a9d5b319e914e897f54613
jaejunday <wowns8114@gmail.com>
2024-10-21 01:07:57 +0900

    feat: 리액션 버튼 추가 feature/TODAK-112/UI-navigation-logo

--- components/Home/Feed.tsx
+++ components/Home/Feed.tsx
@@ -1,11 +1,16 @@
-"use client";
-
-import React from "react";
+// ... 기존 import 문 ...
+import { useState } from "react";
+import { UserAvatarWithLabel } from "../Profile";
 import Image from "next/image";
 import { HR } from "flowbite-react";
-import { UserAvatarWithLabel } from "../Profile";
 
 const Feed: React.FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className="flex-col w-full max-w-md border-b border-gray-200">
       <UserAvatarWithLabel
@@ -13,7 +18,7 @@ const Feed: React.FC = () => {
         nickname="King cat"
         description="2024-10-10"
       />
-      <div className="w-full relative" onClick={() => {}}>
+      <div className="w-full relative">
         <Image
           width={500}
           height={500}
@@ -23,8 +28,37 @@ const Feed: React.FC = () => {
           style={{ width: "100%", height: "auto" }}
           className="rounded-md shadow-md"
         />
+        <button
+          onClick={toggleMenu}
+          className="absolute bottom-2 left-2 bg-gray-100 text-gray-800 text-xs font-medium px-1 py-1 rounded-full dark:bg-gray-700 dark:text-gray-300 opacity-75"
+        >
+          😄
+        </button>
+        {isMenuOpen && (
+          <div className="absolute bottom-2 left-10 bg-white rounded-full shadow-lg flex space-x-2 opacity-75">
+            <button className="block w-full text-left px-1 y-2 hover:bg-gray-100">
+              😀
+            </button>
+            <button className="block w-full text-left px-1 y-2 hover:bg-gray-100">
+              😀
+            </button>
+            <button className="block w-full text-left px-1 y-2 hover:bg-gray-100">
+              😀
+            </button>
+            <button className="block w-full text-left px-1 y-2 hover:bg-gray-100">
+              😀
+            </button>
+            <button className="block w-full text-left px-1 y-2 hover:bg-gray-100 pr-4">
+              😀
+            </button>
+          </div>
+        )}
       </div>
-      <HR className="mb-0" />
+      <div className="mb-10">
+        <p className="w-full border-none text-sm bg-white font-mono m-2">
+          안녕하세요. 고양이 사진입니다.
+        </p>
+      </div>
     </div>
   );
 };
