5b6c4bd4f1fb172f5491b922d6cedb9794294407
jaejunday <wowns8114@gmail.com>
2024-10-22 20:56:13 +0900

    feat: AI 코멘트 모달로 구현 feature/TODAK-123/update-diary

--- store/slices/dairy/diarySlice.ts
+++ store/slices/dairy/diarySlice.ts
@@ -1,14 +1,16 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 // import { addSignupExtraReducers } from "./signupExtraReducers";
 
-export interface LoginState {
+export interface DiaryState {
   date: Date;
+  commentView: boolean;
   loading: any;
   error: any;
 }
 
 export const initialState = {
   date: new Date(),
+  commentView: false,
   loading: false,
   error: "",
 };
@@ -20,10 +22,13 @@ const diarySlice = createSlice({
     setDate: (state, action: PayloadAction<Date>) => {
       state.date = action.payload;
     },
+    setCommentView: (state, action: PayloadAction<boolean>) => {
+      state.commentView = action.payload;
+    },
   },
   //   extraReducers: (builder: any) => addLoginExtraReducers(builder),
 });
 
-export const { setDate } = diarySlice.actions;
+export const { setDate, setCommentView } = diarySlice.actions;
 export const extraReducers = diarySlice.reducer;
 export default diarySlice.reducer;
