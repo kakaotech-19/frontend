16e64638f1e2b5d2116134dc9ffaebebff6e2bd2
jaejunday <wowns8114@gmail.com>
2024-10-23 14:25:35 +0900

    delete: 마이페이지 캘린더 삭제

--- app/(main)/my/page.tsx
+++ app/(main)/my/page.tsx
@@ -46,35 +46,8 @@ const page: React.FC = () => {
         </div>
       </div>
       <div className="flex justify-center items-center">
-        <Calendar
-          prev2Label={null} // 연도 앞으로 이동
-          next2Label={null} // 연도 뒤로 이동
-          showNeighboringMonth={false} // 이전 달, 다음 달 보이기
-          value={date}
-          onChange={onChange}
-          className="w-full max-w-md space-y-2 bg-white border border-gray-200 rounded-md shadow"
-          tileClassName="flex text-center p-4 border border-gray-100 hover:bg-cyan-500 rounded-sm text-gray-700"
-          tileContent={({ date }) => (
-            <span className="flex justify-center items-center w-1 h-1 rounded-full bg-cyan-500 text-white"></span>
-          )}
-          navigationLabel={({ date }) => (
-            <span className="flex text-lg font-semibold p-2">
-              {date.toLocaleString("ko-KR", {
-                month: "long",
-                year: "numeric",
-              })}
-            </span>
-          )}
-        />
+        {/* 인스타 피드 스타일 */}
       </div>
-      <p className="mt-4 text-lg text-gray-700">
-        선택된 날짜:{" "}
-        {date.toLocaleDateString("ko-KR", {
-          year: "numeric",
-          month: "long",
-          day: "numeric",
-        })}
-      </p>
     </div>
   );
 };
