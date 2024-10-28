80e880a4634f4cd3c8be5ac04bdfff7a66359ac6
jaejunday <wowns8114@gmail.com>
2024-10-28 21:27:08 +0900

    feat: 회원 정보

--- store/slices/member/memberExtraReducers.ts
+++ store/slices/member/memberExtraReducers.ts
@@ -1,9 +1,33 @@
-import { ActionReducerMapBuilder } from "@reduxjs/toolkit";
+import { ActionReducerMapBuilder, createAsyncThunk } from "@reduxjs/toolkit";
 import { MemberState } from "./memberSlice";
+import axios from "axios";
+
+// 회원 정보 -----------------------------------------------------
+export const fetchMemberInfo = createAsyncThunk(
+  "member/fetchMemberInfo",
+  async () => {
+    const response = await axios.get("/member/detail");
+    return response.data;
+  }
+);
+
+const addFetchMemberInfo = (builder: ActionReducerMapBuilder<MemberState>) => {
+  builder.addCase(fetchMemberInfo.pending, (state) => {
+    state.loading = true;
+    state.error = null;
+  });
+  builder.addCase(fetchMemberInfo.fulfilled, (state, action) => {
+    state.loading = false;
+  });
+  builder.addCase(fetchMemberInfo.rejected, (state, action) => {
+    state.loading = false;
+    state.error = action.error.message ?? null;
+  });
+};
 
 // extra reducers 추가 -----------------------------------------------------
 export const addMemberExtraReducers = (
   builder: ActionReducerMapBuilder<MemberState>
 ) => {
-  // addLogoutUser(builder);
+  addFetchMemberInfo(builder);
 };
