f383ea1c93c1427679795a365f33b73abb277668
jaejunday <wowns8114@gmail.com>
2024-10-28 20:07:38 +0900

    feat: 로그아웃

--- store/slices/login/loginExtraReducers.ts
+++ store/slices/login/loginExtraReducers.ts
@@ -8,6 +8,12 @@ export const loginUser = createAsyncThunk("login/login", async (data: any) => {
   return response.data;
 });
 
+// 로그아웃 -----------------------------------------------------
+export const logoutUser = createAsyncThunk("login/logout", async () => {
+  const response = await axios.post("/auth/logout");
+  return response.data;
+});
+
 // extra reducers 추가 -----------------------------------------------------
 export const addLoginExtraReducers = (
   builder: ActionReducerMapBuilder<LoginState>
