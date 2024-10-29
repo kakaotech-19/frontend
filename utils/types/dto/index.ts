0349619d0f3620fe84f7a82508b4f2741effd73a
jaejunday <wowns8114@gmail.com>
2024-10-29 23:11:13 +0900

    feat: 배럴 노출패턴 추가 feature/TODAK-172/auth-integration

---
+++ utils/types/dto/index.ts
@@ -0,0 +1,17 @@
+import {
+  VerifyEmailType,
+  VerifyEmailCodeType,
+  CheckNicknameDuplicateType,
+  CheckIDDuplicateType,
+  RegisterUserType,
+  LoginUserType,
+} from "./authRequestType";
+
+export type {
+  VerifyEmailType,
+  VerifyEmailCodeType,
+  CheckNicknameDuplicateType,
+  CheckIDDuplicateType,
+  RegisterUserType,
+  LoginUserType,
+};
