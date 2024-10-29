4d4444aeeb921f344b37c3e70dd385042f3e5d93
jaejunday <wowns8114@gmail.com>
2024-10-29 22:59:38 +0900

    feat: 이메일 인증번호 타입 feature/TODAK-172/auth-integration

--- utils/types/dto/authRequestType.ts
+++ utils/types/dto/authRequestType.ts
@@ -1,4 +1,9 @@
 // 이메일 인증 -----------------------------------------------------
 export type VerifyEmailType = {
-  email: "string";
+  email: string;
+};
+
+// 이메일 인증번호 확인 -----------------------------------------------------
+export type VerifyEmailCodeType = {
+  emailOtp: string;
 };
