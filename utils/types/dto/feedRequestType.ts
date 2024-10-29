07706db732234fa373b375705e014bb6d62afc89
jaejunday <wowns8114@gmail.com>
2024-10-29 23:18:43 +0900

    feat: 일기장 공개 업로드 타입 feature/TODAK-172/auth-integration

--- utils/types/dto/feedRequestType.ts
+++ utils/types/dto/feedRequestType.ts
@@ -3,3 +3,9 @@ export type ReactionFeedType = {
   diaryId: number;
   reactionType: string;
 };
+
+// 일기장 공개 업로드 -----------------------------------------------------
+export type UploadFeedType = {
+  diaryId: number;
+  publicContent: string;
+};
