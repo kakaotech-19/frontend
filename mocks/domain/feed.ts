c147b6ec52145ce7a993d01b831fc9361f6c6c0a
jaejunday <wowns8114@gmail.com>
2024-10-29 20:53:48 +0900

    feat: 무한스크롤 feature/TODAK-163/mockup

--- mocks/domain/feed.ts
+++ mocks/domain/feed.ts
@@ -2,8 +2,97 @@ import { HttpResponse, http } from "msw";
 
 const url = process.env.NEXT_PUBLIC_API_URL;
 export const feedMockups = [
-  // 이메일 인증
-  //   http.post(url + "/auth/email", () => {
-  //     return new HttpResponse("No Content", { status: 204 });
-  //   }),
+  // 일기장 불러오기 (무한 스크롤)
+  http.get(url + "/diary/public?after=5", () => {
+    return new HttpResponse.JSON({
+      diaries: [
+        {
+          publicId: 5, // public diary
+          characterImageUrl: "https://s3-url.com", // member
+          nickname: "todak", // member
+          publicContent: "blah blah", // public diary
+          webtoonImageUrl: "https://s3-url.com", // diary
+          bgmUrl: "https://s3-bgm-url.com", // diary
+          date: "2024-09-21",
+          reactionCount: {
+            // reaction
+            like: 0, // 좋아요
+            surprised: 0, // 놀랐어요
+            empathize: 1, // 공감해요
+            cheering: 0, // 응원해요
+          },
+          myReaction: ["like", "cheering"], // reaction
+        },
+        {
+          publicId: 4, // public diary
+          characterImageUrl: "https://s3-url.com", // member
+          nickname: "todak", // member
+          publicContent: "blah blah", // public diary
+          webtoonImageUrl: "https://s3-url.com", // diary
+          bgmUrl: "https://s3-bgm-url.com", // diary
+          date: "2024-09-21",
+          reactionCount: {
+            // reaction
+            like: 0, // 좋아요
+            surprised: 0, // 놀랐어요
+            empathize: 1, // 공감해요
+            cheering: 0, // 응원해요
+          },
+          myReaction: ["like", "cheering"], // reaction
+        },
+        {
+          publicId: 3, // public diary
+          characterImageUrl: "https://s3-url.com", // member
+          nickname: "todak", // member
+          publicContent: "blah blah", // public diary
+          webtoonImageUrl: "https://s3-url.com", // diary
+          bgmUrl: "https://s3-bgm-url.com", // diary
+          date: "2024-09-21",
+          reactionCount: {
+            // reaction
+            like: 0, // 좋아요
+            surprised: 0, // 놀랐어요
+            empathize: 1, // 공감해요
+            cheering: 0, // 응원해요
+          },
+          myReaction: ["like", "cheering"], // reaction
+        },
+        {
+          publicId: 2, // public diary
+          characterImageUrl: "https://s3-url.com", // member
+          nickname: "todak", // member
+          publicContent: "blah blah", // public diary
+          webtoonImageUrl: "https://s3-url.com", // diary
+          bgmUrl: "https://s3-bgm-url.com", // diary
+          date: "2024-09-21",
+          reactionCount: {
+            // reaction
+            like: 0, // 좋아요
+            surprised: 0, // 놀랐어요
+            empathize: 1, // 공감해요
+            cheering: 0, // 응원해요
+          },
+          myReaction: ["like", "cheering"], // reaction
+        },
+        {
+          publicId: 1, // public diary
+          characterImageUrl: "https://s3-url.com", // member
+          nickname: "todak", // member
+          publicContent: "blah blah", // public diary
+          webtoonImageUrl: "https://s3-url.com", // diary
+          bgmUrl: "https://s3-bgm-url.com", // diary
+          date: "2024-09-21",
+          reactionCount: {
+            // reaction
+            like: 0, // 좋아요
+            surprised: 0, // 놀랐어요
+            empathize: 1, // 공감해요
+            cheering: 0, // 응원해요
+          },
+          myReaction: ["like", "cheering"], // reaction
+        },
+      ],
+      after: 1,
+    });
+  }),
 ];
