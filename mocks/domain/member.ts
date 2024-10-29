5ea9fc37af3532bfa3f08106cea0c7408504e0dc
jaejunday <wowns8114@gmail.com>
2024-10-29 21:08:28 +0900

    feat: 회원 정보 축약 feature/TODAK-163/mockup

--- mocks/domain/member.ts
+++ mocks/domain/member.ts
@@ -10,4 +10,12 @@ export const memberMockups = [
       characterImageUrl: "https://s3-url.com",
     });
   }),
+
+  // 회원 정보 축약
+  http.get(url + "/member/summary", () => {
+    return new HttpResponse.JSON({
+      nickname: "todak",
+      characterImageUrl: "https://s3-url.com",
+    });
+  }),
 ];
