bce43ce372892d213fee10de4d9fed865c7b5d17
jaejunday <wowns8114@gmail.com>
2024-10-29 20:58:35 +0900

    feat: 나의 일기 상세 조회 feature/TODAK-163/mockup

--- mocks/domain/diary.ts
+++ mocks/domain/diary.ts
@@ -6,4 +6,24 @@ export const diaryMockups = [
   //   http.post(url + "/auth/email", () => {
   //     return new HttpResponse("No Content", { status: 204 });
   //   }),
+
+  // 나의 일기 상세 조회
+  http.get(url + "/diary/my/detail?date=2024-10-07", () => {
+    return new HttpResponse.JSON({
+      diaryId: 6,
+      content: "blah blah",
+      // "publicContent" : "public content",
+      webtoonImageUrl: "https://s3-url.com",
+      bgmUrl: "https://s3-url.com",
+      reactionCount: {
+        like: 0,
+        surprised: 0,
+        empathize: 1,
+        cheering: 0,
+      },
+      aiComment: "good good",
+      emotion: "JOY",
+      date: "2024-10-17",
+    });
+  }),
 ];
