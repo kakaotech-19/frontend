3c9c9712d14a08905e583828e61c2158b191ab14
JaeJunday <wowns8114@gmail.com>
2024-10-29 17:36:20 +0900

    feat: 이메일 인증 목업 추가

---
+++ mocks/domain/auth.ts
@@ -0,0 +1,9 @@
+import { HttpResponse, http } from "msw";
+
+const url = process.env.NEXT_PUBLIC_API_URL;
+export const authMockups = [
+  // 이메일 인증
+  http.post(url + "/auth/email", () => {
+    return new HttpResponse("No Content", { status: 204 });
+  }),
+];
