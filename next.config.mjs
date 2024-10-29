7d6ce2718000a795c9f2bd8c9e2100d1dd938455
JaeJunday <wowns8114@gmail.com>
2024-10-29 17:00:41 +0900

    feat: 모킹라이브러리 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias["_http_common"] = false;
+    return config;
+  },
+};
 
 export default nextConfig;
