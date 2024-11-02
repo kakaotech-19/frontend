3ce89e770e8fa38802b0f08a0b4e257e603d973f
jaejunday <wowns8114@gmail.com>
2024-11-02 10:27:22 +0900

    refactor: 피드 목업 개선 feature/TODAK-188/feed-intergration

--- feature/mocks/domain/feed.ts
+++ feature/mocks/domain/feed.ts
@@ -1,102 +1,36 @@
 import { HttpResponse, http } from "msw";
 
 const url = process.env.NEXT_PUBLIC_API_URL;
+
+// 목업 데이터 생성을 위한 헬퍼 함수
+const createMockDiary = (id: number) => ({
+  publicDiaryId: id,
+  diaryId: id + 8,
+  characterImageUrl: "/minion1.png",
+  nickname: "todak",
+  publicContent: "blah blah",
+  webtoonImageUrls: [
+    "/minion1.png",
+    "/minion2.png",
+    "minion3.png",
+    "minion4.png",
+  ],
+  bgmUrl: "https://s3-bgm-url.com",
+  date: "2024-09-21",
+  reactionCount: {
+    like: 0,
+    surprised: 0,
+    empathize: 1,
+    cheering: 0,
+  },
+  myReaction: ["like", "cheering"],
+});
+
 export const feedMockups = [
   // 일기장 불러오기 (무한 스크롤)
   http.get(url + "/diary/public", () => {
     return HttpResponse.json({
-      diaries: [
-        {
-          publicDiaryId: 5, // public diary,
-          diaryId: 13, // diary,
-          characterImageUrl: "https://s3-url.com", // member
-          nickname: "todak", // member
-          publicContent: "blah blah", // public diary
-          webtoonImageUrls: ["cut1_url", "cur2_url", "cut3_url", "cur4_url"], // diary
-          bgmUrl: "https://s3-bgm-url.com", // diary
-          date: "2024-09-21", // diary
-          reactionCount: {
-            // reaction
-            like: 0, // 좋아요
-            surprised: 0, // 놀랐어요
-            empathize: 1, // 공감해요
-            cheering: 0, // 응원해요
-          },
-          myReaction: ["like", "cheering"], // reaction
-        },
-        {
-          publicDiaryId: 4, // public diary,
-          diaryId: 11, // diary,
-          characterImageUrl: "https://s3-url.com", // member
-          nickname: "todak", // member
-          publicContent: "blah blah", // public diary
-          webtoonImageUrls: ["cut1_url", "cur2_url", "cut3_url", "cur4_url"], // diary
-          bgmUrl: "https://s3-bgm-url.com", // diary
-          date: "2024-09-21",
-          reactionCount: {
-            // reaction
-            like: 0, // 좋아요
-            surprised: 0, // 놀랐어요
-            empathize: 1, // 공감해요
-            cheering: 0, // 응원해요
-          },
-          myReaction: ["like", "cheering"], // reaction
-        },
-        {
-          publicDiaryId: 3, // public diary,
-          diaryId: 10, // diary,
-          characterImageUrl: "https://s3-url.com", // member
-          nickname: "todak", // member
-          publicContent: "blah blah", // public diary
-          webtoonImageUrls: ["cut1_url", "cur2_url", "cut3_url", "cur4_url"], // diary
-          bgmUrl: "https://s3-bgm-url.com", // diary
-          date: "2024-09-21",
-          reactionCount: {
-            // reaction
-            like: 0, // 좋아요
-            surprised: 0, // 놀랐어요
-            empathize: 1, // 공감해요
-            cheering: 0, // 응원해요
-          },
-          myReaction: ["like", "cheering"], // reaction
-        },
-        {
-          publicDiaryId: 2, // public diary,
-          diaryId: 9, // diary,
-          characterImageUrl: "https://s3-url.com", // member
-          nickname: "todak", // member
-          publicContent: "blah blah", // public diary
-          webtoonImageUrls: ["cut1_url", "cur2_url", "cut3_url", "cur4_url"], // diary
-          bgmUrl: "https://s3-bgm-url.com", // diary
-          date: "2024-09-21",
-          reactionCount: {
-            // reaction
-            like: 0, // 좋아요
-            surprised: 0, // 놀랐어요
-            empathize: 1, // 공감해요
-            cheering: 0, // 응원해요
-          },
-          myReaction: ["like", "cheering"], // reaction
-        },
-        {
-          publicDiaryId: 1, // public diary,
-          diaryId: 8, // diary,
-          characterImageUrl: "https://s3-url.com", // member
-          nickname: "todak", // member
-          publicContent: "blah blah", // public diary
-          webtoonImageUrls: ["cut1_url", "cur2_url", "cut3_url", "cur4_url"], // diary
-          bgmUrl: "https://s3-bgm-url.com", // diary
-          date: "2024-09-21",
-          reactionCount: {
-            // reaction
-            like: 0, // 좋아요
-            surprised: 0, // 놀랐어요
-            empathize: 1, // 공감해요
-            cheering: 0, // 응원해요
-          },
-          myReaction: ["like", "cheering"], // reaction
-        },
-      ],
+      diaries: [5, 4, 3, 2, 1].map(createMockDiary),
       after: 1,
     });
   }),
