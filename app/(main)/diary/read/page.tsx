3796c1795728aa7b933d10bc4fc2b26eca4c63d5
jaejunday <wowns8114@gmail.com>
2024-11-03 21:11:29 +0900

    feat: 일기 조회 API 연동 feature/TODAK-191/diary-intergration

--- app/(main)/diary/read/page.tsx
+++ app/(main)/diary/read/page.tsx
@@ -4,11 +4,16 @@ import React, { useEffect } from "react";
 import Image from "next/image";
 import { Button, Modal } from "flowbite-react";
 import { useSearchParams } from "next/navigation";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchDiaryDetail } from "@/feature/redux/slices/dairy/diaryExtraReducers";
+import { RootState } from "@/feature/redux";
+import AudioModule from "@/components/home/AudioModule";
 
 const Page: React.FC = () => {
   const dispatch = useDispatch();
+  const queriedDiary = useSelector(
+    (state: RootState) => state.diary.queriedDiary
+  );
   const [showModal, setShowModal] = React.useState(false);
   const searchParams = useSearchParams();
   const date = searchParams.get("date");
@@ -20,51 +25,37 @@ const Page: React.FC = () => {
   }, [date]);
 
   return (
-    <div className="w-full min-h-screen flex justify-center items-center">
+    <div className="w-full min-h-screen flex justify-center items-start">
       <div className="w-full max-w-md flex flex-col items-center mt-14">
         <div className="flex w-full max-w-md relative" onClick={() => {}}>
           <Image
             width={500}
             height={500}
-            src={"/cat.png"}
+            src={queriedDiary.webtoonImageUrl}
             alt={"게시물 이미지"}
             sizes="100vw"
             style={{ width: "100%", height: "auto" }}
             className="rounded-md shadow-md"
           />
+          <AudioModule src={queriedDiary.bgmUrl} />
           <div
             onClick={() => setShowModal(true)}
-            className="absolute bottom-2 right-2 text-sm font-semibold rounded-full pl-2 pr-2 bg-white opacity-75"
+            className="absolute bottom-2 left-2 text-sm font-semibold rounded-full pl-2 pr-2 bg-white opacity-75"
           >
             AI 코멘트
           </div>
         </div>
         <div className="w-full max-w-md">
           <p className="text-md h-full p-2 rounded-none overflow-y-auto">
-            안녕하세요. 여기는 일기 내용입니다. 일기내용 안녕하세요. 여기는 일기
-            내용입니다. 일기내용 안녕하세요. 여기는 일기 내용입니다. 일기내용
-            안녕하세요. 여기는 일기 내용입니다. 일기내용 안녕하세요. 여기는 일기
-            내용입니다. 일기내용 안녕하세요. 여기는 일기 내용입니다. 일기내용
-            안녕하세요. 여기는 일기 내용입니다. 일기내용 안녕하세요. 여기는 일기
-            내용입니다. 일기내용 안녕하세요. 여기는 일기 내용입니다. 일기내용
-            안녕하세요. 여기는 일기 내용입니다. 일기내용 안녕하세요. 여기는 일기
-            내용입니다. 일기내용 안녕하세요. 여기는 일기 내용입니다. 일기내용
-            안녕하세요. 여기는 일기 내용입니다. 일기내용 안녕하세요. 여기는 일기
-            내용입니다. 일기내용 안녕하세요. 여기는 일기 내용입니다. 일기내용
-            안녕하세요. 여기는 일기 내용입니다. 일기내용 안녕하세요. 여기는 일기
-            내용입니다. 일기내용 안녕하세요. 여기는 일기 내용입니다. 일기내용
-            안녕하세요. 여기는 일기 내용입니다. 일기내용 안녕하세요. 여기는 일기
-            내용입니다. 일기내용 안녕하세요. 여기는 일기 내용입니다. 일기내용
-            안녕하세요. 여기는 일기 내용입니다. 일기내용 안녕하세요. 여기는 일기
-            내용입니다. 일기내용 안녕하세요. 여기는 일기 내용입니다. 일기내용
+            {queriedDiary.content}
           </p>
         </div>
         <Modal show={showModal} onClose={() => setShowModal(false)}>
-          <Modal.Header>토닥토닥</Modal.Header>
+          <Modal.Header>AI 코멘트</Modal.Header>
           <Modal.Body>
             <div className="space-y-6">
               <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                여기에 모달 내용을 넣으세요.
+                {queriedDiary.aiComment}
               </p>
             </div>
           </Modal.Body>
