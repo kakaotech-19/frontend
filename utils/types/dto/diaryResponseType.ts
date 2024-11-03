c66da8cedc271bcc5f383ab415090f097f595070
jaejunday <wowns8114@gmail.com>
2024-11-03 21:11:16 +0900

    comment: 변경점 주석 추가 feature/TODAK-191/diary-intergration

--- utils/types/dto/diaryResponseType.ts
+++ utils/types/dto/diaryResponseType.ts
@@ -4,9 +4,9 @@ export type DiaryResponseType = {
   diaryId: number;
   content: string;
   // "publicContent" : "public content",
-  webtoonImageUrl: string;
+  webtoonImageUrl: string; // legacy: 이미지 리스트로 변경되어야함
   bgmUrl: string;
-  reactionCount: ReactionType;
+  reactionCount: ReactionType; // legacy: 제거 되어야함
   aiComment: string;
   emotion: string;
   date: string;
