a9026ad767fa904ba484c7dff84bc9a5e03438a8
jaejunday <wowns8114@gmail.com>
2024-11-04 15:24:49 +0900

    feat: 공용 날짜 검증 로직 추가 feature/TODAK-197/update-mockup

--- domain/shared/function/index.ts
+++ domain/shared/function/index.ts
@@ -2,4 +2,13 @@ import encodeFileToBase64 from "./encodeBase64";
 import isValidEmail from "./isValidEmail";
 import isValidAccessToken from "./isValidAccessToken";
 import isValidDate from "./isValidDate";
-export { encodeFileToBase64, isValidEmail, isValidAccessToken, isValidDate };
+import isValidNickname from "./isValidNickname";
+import isValidDiaryId from "./isValidDiaryId";
+export {
+  encodeFileToBase64,
+  isValidEmail,
+  isValidAccessToken,
+  isValidDate,
+  isValidNickname,
+  isValidDiaryId,
+};
