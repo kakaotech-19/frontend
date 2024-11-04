01101e36a07612ef8ef91cd5f9f05653b8896ef4
jaejunday <wowns8114@gmail.com>
2024-11-04 14:39:24 +0900

    feat: 다이어리 도메인 요청 dto 타입 배럴패턴 구현 feature/TODAK-197/update-mockup

---
+++ domain/diary/dto/request/index.ts
@@ -0,0 +1,12 @@
+import {
+  CreateDiaryEntryRequestDto,
+  CreateDiaryEntryType,
+} from "./createDiaryDto";
+
+import {
+  DeleteDiaryEntryRequestDto,
+  DeleteDiaryEntryType,
+} from "./deleteDiaryDto";
+
+export { CreateDiaryEntryRequestDto, DeleteDiaryEntryRequestDto };
+export type { CreateDiaryEntryType, DeleteDiaryEntryType };
