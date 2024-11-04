a93daebf4d1a22186127de837583d8479fc9da37
jaejunday <wowns8114@gmail.com>
2024-11-04 15:07:27 +0900

    feat: 피드 요청 dto 배럴 패턴 구현 feature/TODAK-197/update-mockup

---
+++ domain/feed/dto/request/index.ts
@@ -0,0 +1,5 @@
+import { ReactionFeedRequestDto, ReactionFeedType } from "./reactionFeedDto";
+import { UploadFeedRequestDto, UploadFeedType } from "./uploadFeedDto";
+
+export { ReactionFeedRequestDto, UploadFeedRequestDto };
+export type { ReactionFeedType, UploadFeedType };
