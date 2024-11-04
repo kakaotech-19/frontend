c4100ce4eb80732cf249965f354734f54a0b4bcf
jaejunday <wowns8114@gmail.com>
2024-11-04 15:23:46 +0900

    feat: 닉네임 검사 요청 dto feature/TODAK-197/update-mockup

--- domain/auth/dto/request/checkNicknameDuplicateDto.ts
+++ domain/auth/dto/request/checkNicknameDuplicateDto.ts
@@ -1,3 +1,5 @@
+import { isValidNickname } from "@/domain/shared/function";
+
 // 닉네임 중복 확인 타입 정의 -----------------------------------------------------
 export type CheckNicknameDuplicateType = {
   nickname: string;
@@ -10,24 +12,11 @@ export class CheckNicknameDuplicateRequestDto
   public nickname: string;
 
   constructor(nickname: string) {
-    if (!this.isValidNickname(nickname)) {
+    if (!isValidNickname(nickname)) {
       throw new Error("Invalid nickname format.");
     }
 
     this.nickname = nickname.trim(); // 닉네임의 앞뒤 공백 제거
-  }
-
-  // 닉네임 형식 검증 메서드
-  private isValidNickname(nickname: string): boolean {
-    const minNicknameLength = 3;
-    const maxNicknameLength = 15;
-
-    // 최소/최대 길이와 공백 없는 형식 체크
-    return (
-      nickname.length >= minNicknameLength &&
-      nickname.length <= maxNicknameLength &&
-      /^[a-zA-Z0-9가-힣]+$/.test(nickname) // 영문, 숫자, 한글로만 구성
-    );
   }
 
   // 객체 형태로 반환
