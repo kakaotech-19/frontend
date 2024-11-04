c91a9c9a5790ed7233b125ef2a3dc009e872fef0
jaejunday <wowns8114@gmail.com>
2024-11-04 14:12:38 +0900

    feat: 토큰 재발급 응답 dto feature/TODAK-197/update-mockup

---
+++ domain/auth/dto/response/reIssueTokenDto.ts
@@ -0,0 +1,26 @@
+import { isValidAccessToken } from "@/domain/shared/function";
+
+// 토큰 재발급 응답 타입 정의 -----------------------------------------------------
+export type ReIssueTokenType = {
+  accessToken: string;
+};
+
+// 토큰 재발급 응답 DTO 클래스 -----------------------------------------------------
+export class ReIssueTokenResponseDto implements ReIssueTokenType {
+  public accessToken: string;
+
+  constructor(accessToken: string) {
+    if (!isValidAccessToken(accessToken)) {
+      throw new Error("Invalid access token format.");
+    }
+
+    this.accessToken = accessToken;
+  }
+
+  // 객체 형태로 반환
+  toObject(): ReIssueTokenType {
+    return {
+      accessToken: this.accessToken,
+    };
+  }
+}
