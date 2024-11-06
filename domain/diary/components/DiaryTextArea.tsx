ca64e53e0ddbc98e1bac345b2234d0787ad73c4e
jaejunday <wowns8114@gmail.com>
2024-11-06 13:28:48 +0900

    feat: 다이어리 입력 포맷 분리 feature/TODAK-213/update-testcase

---
+++ domain/diary/components/DiaryTextArea.tsx
@@ -0,0 +1,23 @@
+import { Textarea } from "flowbite-react";
+
+interface DiaryTextAreaProps {
+  text: string;
+  onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+}
+
+export const DiaryTextArea = ({ text, onChange }: DiaryTextAreaProps) => {
+  return (
+    <div className="relative mt-4">
+      <Textarea
+        className="h-96 text-md"
+        maxLength={3000}
+        value={text}
+        onChange={onChange}
+        placeholder="오늘의 이야기를 들려주세요..."
+      />
+      <div className="absolute bottom-4 right-4 text-sm text-gray-500">
+        {text.length} / 3000
+      </div>
+    </div>
+  );
+};
