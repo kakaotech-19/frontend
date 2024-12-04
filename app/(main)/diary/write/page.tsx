969bea0f814d984aeeec2b1423c0552065e6d817
kchranger <kchranger@gmail.com>
2024-12-04 10:19:11 +0900

    fix: 일기 작성시 새로고침을 하면 에러 발생 해결 TODAK-277
    - 초단위 에서 분단위 시간 제공을 통해 에러 해결

--- app/(main)/diary/write/page.tsx
+++ app/(main)/diary/write/page.tsx
@@ -110,7 +110,7 @@ const DiaryWritePage: React.FC = () => {
             <div>
               <p className="text-xs text-gray-500 ml-2">임시저장</p>
               <p className="text-xs text-gray-500 ml-2">
-                {date.toTimeString().slice(0, 8)}
+                {date.toTimeString().slice(0, 5)}
               </p>
             </div>
           </div>
