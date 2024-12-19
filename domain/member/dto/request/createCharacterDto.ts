68df12375bbb3ed70037548539c15d3da30b0dd1
Sin Han <vrdhan212@gmail.com>
2024-12-19 13:41:36 +0900

    refactor: 캐릭터 생성 dto에 화풍 추가

--- domain/member/dto/request/createCharacterDto.ts
+++ domain/member/dto/request/createCharacterDto.ts
@@ -1,20 +1,26 @@
+import { CHARACTER_STYLE_VALUES } from "@/domain/diary/constants/characterStyle";
+
 // 멀티파트 이미지 전송을 위한 타입 정의
 export type CreateCharacterType = {
   image: File; // 멀티파트 이미지 타입으로 File 사용
+  characterStyle: string;
 };
 
 // DTO 클래스 정의
 export class CreateCharacterRequestDto implements CreateCharacterType {
   public image: File;
+  public characterStyle: string;
 
-  constructor({ image }: CreateCharacterType) {
+  constructor({ image, characterStyle }: CreateCharacterType) {
     this.image = image;
+    this.characterStyle = CHARACTER_STYLE_VALUES[characterStyle];
   }
 
   // 객체 형태로 반환
   toObject(): CreateCharacterType {
     return {
       image: this.image,
+      characterStyle: this.characterStyle,
     };
   }
 
@@ -22,6 +28,7 @@ export class CreateCharacterRequestDto implements CreateCharacterType {
   toFormData(): FormData {
     const formData = new FormData();
     formData.append("uploadImage", this.image); // 'image' 필드에 파일 추가
+    formData.append("characterStyle", this.characterStyle);
     return formData;
   }
 }
