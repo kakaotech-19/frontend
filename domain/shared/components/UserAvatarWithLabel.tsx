25a9271128c8f1c5921c3ce5834b0d2e3276abbb
kchranger <kchranger@gmail.com>
2024-12-24 12:25:01 +0900

    feat: 피드 날짜 UI 형식 수정
    - "UTC ISO" 에서 "년-월-일" 로 변경

--- domain/shared/components/UserAvatarWithLabel.tsx
+++ domain/shared/components/UserAvatarWithLabel.tsx
@@ -2,36 +2,36 @@
 
 import React from "react";
 import Image from "next/image";
-import { Label } from "flowbite-react";
+import {Label} from "flowbite-react";
 
 interface UserAvatarWithLabelProps {
-  imageUrl?: string;
-  nickname: string;
-  description?: string;
+    imageUrl?: string;
+    nickname: string;
+    description?: string;
 }
 
 const UserAvatarWithLabel: React.FC<UserAvatarWithLabelProps> = ({
-  imageUrl,
-  nickname,
-  description,
-}) => {
-  return (
-    <div className="flex justify-start m-2 space-x-4 items-ends">
-      <Image
-        className="w-10 h-10 rounded-full flex items-center"
-        src={imageUrl || "/cat.png"}
-        width={50}
-        height={50}
-        alt="Rounded avatar"
-      />
-      <div className="flex-col">
-        <Label className="flex items-end text-md">{nickname}</Label>
-        <Label className="flex items-end text-xs text-gray-500 dark:text-gray-400">
-          {description}
-        </Label>
-      </div>
-    </div>
-  );
+                                                                     imageUrl,
+                                                                     nickname,
+                                                                     description,
+                                                                 }) => {
+    return (
+        <div className="flex justify-start m-2 space-x-4 items-ends">
+            <Image
+                className="w-10 h-10 rounded-full flex items-center"
+                src={imageUrl || "/cat.png"}
+                width={50}
+                height={50}
+                alt="Rounded avatar"
+            />
+            <div className="flex-col">
+                <Label className="flex items-end text-md">{nickname}</Label>
+                <Label className="flex items-end text-xs text-gray-500 dark:text-gray-400">
+                    {description?.slice(0, 10)}
+                </Label>
+            </div>
+        </div>
+    );
 };
 
 export default UserAvatarWithLabel;
